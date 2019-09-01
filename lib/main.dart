0db8a5aa61dfb312bc4ea95edcb91069d3e2f5cf
AMOREM <amorem@amorem.com>
2019-09-01 16:06:23 +0200

    ListView/ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Stack',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Stack'),
+          title: Text('ListView • ListTile'),
         ),
         backgroundColor: Colors.indigo[100],
         body: Home(),
@@ -24,14 +24,11 @@ class Home extends StatelessWidget {
     final sizeX = MediaQuery.of(context).size.width;
     final sizeY = MediaQuery.of(context).size.height;
     return Container(
-      width: sizeX,
-      height: sizeY,
-      child: ListView.separated(
-        itemCount: 25,
-        itemBuilder: (context, index) => createSquare(index),
-        separatorBuilder: (context, index) => createSeparator(index),
-      ),
-    );
+        width: sizeX,
+        height: sizeY,
+        child: ListView(
+          children: showContacts(),
+        ));
   }
 }
 
@@ -153,4 +150,60 @@ Widget createSquare(int position) {
 Widget createSeparator(int position) {
   Widget separator = Container(height: 15, color: Colors.black);
   return separator;
+}
+
+class Contact {
+  String name;
+  String subtitle;
+  IconData icon;
+
+  Contact(this.name, this.subtitle, this.icon);
+}
+
+List<Contact> buildContacts() {
+  List<Contact> contacts = List<Contact>();
+  contacts.add(
+    Contact('Thomas Anderson', 'The Matrix', Icons.sentiment_neutral),
+  );
+  contacts.add(
+    Contact('Frank Slade', 'Scent of a Woman', Icons.sentiment_satisfied),
+  );
+  contacts.add(
+    Contact('Mildred Hayes', 'Three billboards outside ebbing',
+        Icons.sentiment_dissatisfied),
+  );
+  contacts.add(
+    Contact('Bruce Wayne', 'The DarK Night', Icons.sentiment_neutral),
+  );
+  contacts.add(
+    Contact('Jamal Malik', 'The Millionaire', Icons.sentiment_satisfied),
+  );
+  return contacts;
+}
+
+List<ListTile> showContacts() {
+  List<Contact> contacts = buildContacts();
+  for (int i = 0; i < 20; i++) {
+    contacts.addAll(buildContacts());
+  }
+  List<ListTile> list = List<ListTile>();
+  contacts.forEach(
+    (contact) {
+      list.add(
+        ListTile(
+          title: Text(contact.name),
+          subtitle: Text(contact.subtitle),
+          leading: CircleAvatar(
+            child: Icon(
+              contact.icon,
+            ),
+            backgroundColor: Colors.amber[600],
+          ),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {},
+        ),
+      );
+    },
+  );
+  return list;
 }
